daa5bdcb95fcaa8004ae8901cf106c4d510b4912
Scott Smith <scott@scottosmith.net>
2020-06-08 11:26:12 -0700

    Adds timestamping to posts and users, clean up date code with a helper

--- models/post.js
+++ models/post.js
@@ -1,23 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-    title: {
-        type: String,
-        required: true
+const postSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        body: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
     },
-    body: {
-        type: String,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model('Post', postSchema);
--- models/user.js
+++ models/user.js
@@ -1,33 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    firstName: {
-        type: String,
-        required: true
+const userSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        fullName: {
+            type: String,
+            required: false
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        authoredPosts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Post'
+            }
+        ]
     },
-    lastName: {
-        type: String,
-        required: true
-    },
-    fullName: {
-        type: String,
-        required: false
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    authoredPosts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Post'
-        }
-    ]
-});
+    { timestamps: true }
+);
 
 module.exports = mongoose.model('User', userSchema);
