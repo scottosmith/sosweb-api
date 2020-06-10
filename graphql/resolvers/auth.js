7ac724a4f9ce7d4289a14dee6e8a2803fe4dd108
Scott Smith <scott@scottosmith.net>
2020-06-10 16:04:24 -0700

    Finished auth resolver

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,15 +1,26 @@
 import bcrypt from 'bcryptjs';
 import User from '../../models/user';
+import jwt from 'jsonwebtoken';
 
 export const login = async ({email, password}) => {
     try {
-        const user = User.findOne({email: email});
+        const user = await User.findOne({email: email});
         if (!user) {
             throw new Error("Login failed!");
         }
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             throw new Error("Login failed!");
+        }
+        const token = jwt.sign(
+            { userId: user.id, email: user.email }, 
+            process.env.AUTH_HASH, 
+            { expiresIn: '3h' }
+        );
+        return {
+            userId: user.id,
+            token: token,
+            tokenExpiration: 3
         }
     } 
     catch (error) {
